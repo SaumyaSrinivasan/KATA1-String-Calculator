af2cc80ba7e597b7b306df2314bead9d7acb48ec
SaumyaSrinivasan <52213259+SaumyaSrinivasan@users.noreply.github.com>
2020-11-21 12:04:32 +0530

    Reducing the extra else part

--- Calculator.java
+++ Calculator.java
@@ -5,14 +5,12 @@ public class Calculator{
          if(numbers.isEmpty()){
               return 0;
          }
-         else if(numbers.contains(",")){
+         else {
                String digits[]=numbers.split(",");
                List<Integer> list=convert(digits , toIntValue());
               return sum(list).intValue();
          }
-         else{ 
-              return toIntValue(numbers);
-         }
+         
      }
      
      private static Converter<String,Integer> toIntValue(){
